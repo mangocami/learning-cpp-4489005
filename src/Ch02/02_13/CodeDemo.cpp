26f5bae3c8d78751bce27ad331d7213f5957b6f5
mangocami <115692742+mangocami@users.noreply.github.com>
2025-02-16 18:04:51 +0000

    challenge ch 2 average

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -8,8 +8,15 @@ int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
-    // Write your code here
-    
+    result = (nums[0]+nums[1]+nums[2]+nums[3]+nums[4])/5;
+    std::cout << "The average is " << result << std::endl;
+
+    result = (nums[0]+nums[1]+nums[2]+nums[3]+nums[4])/5.0;
+    std::cout << "The average is " << result << std::endl;
+
+
+    result = (nums[0]+nums[1]+nums[2]+nums[3]+nums[4]);
+    result /= 5;
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
